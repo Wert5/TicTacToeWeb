26db60ac115d04219c6a8544cb4ead386af7229b
Wert5 <grenier.winston@gmail.com>
2016-03-14 13:26:19 -0400

    pauldwrona push changes

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python
+\#!/usr/bin/python
 
 # Turn on debug mode.
 import cgitb
+import cgi
+import time
+
 cgitb.enable()
 
 # Print necessary headers.
 print("Content-Type: text/html")
-print()
 
 # Connect to the database.
 import pymysql
@@ -18,12 +20,30 @@ conn = pymysql.connect(
 c = conn.cursor()
 
 # Insert some example data.
-c.execute("INSERT INTO numbers VALUES (1, 'One!')")
-c.execute("INSERT INTO numbers VALUES (2, 'Two!')")
-c.execute("INSERT INTO numbers VALUES (3, 'Three!')")
+#c.execute("UPDATE tictactoe SET player='o' WHERE player='x';")
+#conn.commit()
+#c.execute("UPDATE tictactoe SET player='o' WHERE player='x';")
+#conn.commit()
+c.execute("UPDATE tictactoe SET player='blank' WHERE player=' ';")
+
 conn.commit()
 
 # Print the contents of the database.
-c.execute("SELECT * FROM numbers")
-print("<button type=\"button\">Hello</button>")
-//print([(r[0], r[1]) for r in c.fetchall()])
+c.execute("SELECT * FROM tictactoe")
+#print("<button type=\"button\">Hello</button>")
+
+#form = cgi.FieldStorage()
+
+print([(r[0], r[1], r[2]) for r in c.fetchall()])
+#print("<br/>")
+print("<form action=\"index.py\">")
+print("<textarea name=\"textinput\">This is the value</textarea>")
+print("<button type=\"submit\">Submit</button>")
+print("</form>")
+
+#for key in sorted(form.keys())
+#	print(key)
+#        c.execute("INSERT INTO ")
+#	conn.commit()
+#print("<input type=\"text\" />")
+#print("<button type=\"button\">GO</button>")
